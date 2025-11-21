19b9644e59f210f7321b6c29b29a64ac40aba996
Murad Isgenderov <128960062+Muradwebdev@users.noreply.github.com>
2025-11-21 14:36:26 +0400

    Add 'list-watched' class to WatchedMoviesList

--- src/components/WatchedMoviesList.jsx
+++ src/components/WatchedMoviesList.jsx
@@ -2,7 +2,7 @@ import WatchedMovie from "./WatchedMovie";
 
 function WatchedMoviesList({ watched ,handleDeleteMovie}) {
   return (
-    <ul className="list">
+    <ul className="list list-watched">
       {watched &&
         watched.map((movie) => (
           <WatchedMovie movie={movie} key={movie.imdbID} handleDeleteMovie={handleDeleteMovie} />
